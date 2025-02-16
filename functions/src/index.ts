da01b05ea3fda0d1d27e0e3ed5ceac648931dd89
Jared Horwitz <horwitzjared15@gmail.com>
2025-02-16 18:07:52 -0500

    Add node function debugging

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,39 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// /**
+//  * Import function triggers from their respective submodules:
+//  *
+//  * import {onCall} from "firebase-functions/v2/https";
+//  * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+//  *
+//  * See a full list of supported triggers at https://firebase.google.com/docs/functions
+//  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// // The Cloud Functions for Firebase SDK to set up triggers and logging.
+// const {onSchedule} = require("firebase-functions/v2/scheduler");
+// const {logger} = require("firebase-functions");
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+// // The Firebase Admin SDK to delete inactive users.
+// const admin = require("firebase-admin");
+// admin.initializeApp();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// // The es6-promise-pool to limit the concurrency of promises.
+// const PromisePool = require("es6-promise-pool").default;
+// // Maximum concurrent account deletions.
+// const MAX_CONCURRENT = 3;
+
+
+
+// // Run once a day at midnight, to clean up the users
+// // Manually run the task here https://console.cloud.google.com/cloudscheduler
+// exports.accountcleanup = onSchedule("every day 00:00", async (event) => {
+//     // Fetch all user details.
+//     const inactiveUsers = await getInactiveUsers();
+  
+//     // Use a pool so that we delete maximum `MAX_CONCURRENT` users in parallel.
+//     const promisePool = new PromisePool(
+//         () => deleteInactiveUser(inactiveUsers),
+//         MAX_CONCURRENT,
+//     );
+//     await promisePool.start();
+  
+//     logger.log("User cleanup finished");
+//   });
