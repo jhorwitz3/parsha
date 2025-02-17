2b2e0d5bee769488dceb2d765f2569d225b73c5a
Jared Horwitz <horwitzjared15@gmail.com>
2025-02-17 16:36:33 -0500

    update functions

--- functions/src/flow.ts
+++ functions/src/flow.ts
@@ -1,45 +1,40 @@
 // import the Genkit and Google AI plugin libraries
-import { gemini20Flash, googleAI } from '@genkit-ai/googleai';
-import { genkit } from 'genkit';
-import { z } from "zod";
-import { onCallGenkit } from 'firebase-functions/https';
-import {defineSecret} from "firebase-functions/params";
+import {gemini20Flash, googleAI} from "@genkit-ai/googleai";
+import {genkit} from "genkit";
+import {z} from "zod";
 
-const googleAIapiKey = defineSecret("GOOGLE_GENAI_API_KEY");
 
 // configure a Genkit instance
 const ai = genkit({
-  plugins: [googleAI()],
-  model: gemini20Flash, // set default model
+    plugins: [googleAI()],
+    model: gemini20Flash, // set default model
 });
 
 const ParshaSchema = z.object({
     name: z.string(),
     description: z.string(),
     characters: z.array(z.string())
-  });
-  
-  export const parshaDescriptionFlow = ai.defineFlow(
+});
+
+export const parshaDescriptionFlow = ai.defineFlow(
     {
-      name: 'parshaDescriptionFlow',
-      outputSchema: ParshaSchema,
+        name: "parshaDescriptionFlow",
+        outputSchema: ParshaSchema,
     },
     async () => {
-      const { output } = await ai.generate({
-        model: gemini20Flash,
-        prompt: `Give the name of this weeks parsha, a description of the story of the parsha, and identify all key characters, providing brief bios of each character `,
-        output: { schema: ParshaSchema },
-      });
-      if (output == null) {
-        throw new Error("Response doesn't satisfy schema.");
-      }
-      return output;
+        const {output} = await ai.generate({
+            model: gemini20Flash,
+            prompt: `Give the name of this weeks parsha, a description of the story of the parsha, and identify all key characters, providing brief bios of each character `,
+            output: {schema: ParshaSchema},
+        });
+        if (output == null) {
+            throw new Error("Response doesn't satisfy schema.");
+        }
+        return output;
     }
-  );
+);
 
-export const generateParsha = onCallGenkit(
-    {secrets: [googleAIapiKey]},
-    parshaDescriptionFlow);
+
 
 
 // async function main() {
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,39 +1,39 @@
-// /**
-//  * Import function triggers from their respective submodules:
-//  *
-//  * import {onCall} from "firebase-functions/v2/https";
-//  * import {onDocumentWritten} from "firebase-functions/v2/firestore";
-//  *
-//  * See a full list of supported triggers at https://firebase.google.com/docs/functions
-//  */
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-// // The Cloud Functions for Firebase SDK to set up triggers and logging.
+// The Cloud Functions for Firebase SDK to set up triggers and logging.
+
+import {onCallGenkit} from "firebase-functions/https";
+import {defineSecret} from "firebase-functions/params";
+import {parshaDescriptionFlow} from "./flow";
+import admin from "firebase-admin";
+
+const googleAIapiKey = defineSecret("GOOGLE_GENAI_API_KEY");
 // const {onSchedule} = require("firebase-functions/v2/scheduler");
 // const {logger} = require("firebase-functions");
 
-// // The Firebase Admin SDK to delete inactive users.
-// const admin = require("firebase-admin");
-// admin.initializeApp();
+// The Firebase Admin SDK to delete inactive users.
+admin.initializeApp();
 
-// // The es6-promise-pool to limit the concurrency of promises.
-// const PromisePool = require("es6-promise-pool").default;
-// // Maximum concurrent account deletions.
-// const MAX_CONCURRENT = 3;
-
-
-
-// // Run once a day at midnight, to clean up the users
-// // Manually run the task here https://console.cloud.google.com/cloudscheduler
-// exports.accountcleanup = onSchedule("every day 00:00", async (event) => {
+// Run once a day at midnight, to clean up the users
+// Manually run the task here https://console.cloud.google.com/cloudscheduler
+// exports.getparsha = onSchedule("every day 00:00", async (event:any) => {
 //     // Fetch all user details.
-//     const inactiveUsers = await getInactiveUsers();
-  
-//     // Use a pool so that we delete maximum `MAX_CONCURRENT` users in parallel.
-//     const promisePool = new PromisePool(
-//         () => deleteInactiveUser(inactiveUsers),
-//         MAX_CONCURRENT,
-//     );
-//     await promisePool.start();
-  
-//     logger.log("User cleanup finished");
+//     const schema = await parshaDescriptionFlow();
+
+//     logger.log("schema");
+//     logger.log("name: %s", schema.name);
+//     logger.log("description: %s", schema.description);
+//     logger.log("characters: %s", schema.characters);
+//     logger.log("event: %s", event);
 //   });
+
+export const generateParsha = onCallGenkit(
+{secrets: [googleAIapiKey]},
+parshaDescriptionFlow);
