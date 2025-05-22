5b90f81a18d90d367eee9d85f5db4557f08d4a2f
Jared Horwitz <horwitzjared15@gmail.com>
2025-05-22 16:09:18 -0400

    generate an image for each part of the parsha

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -80,7 +80,8 @@ async (event:ScheduledEvent) => {
   logger.log(`Parashat: ${data.name} added now.`);
 });
 
-exports.genImageUrl = onRequest({secrets: [apiKey], timeoutSeconds: 500, memory: "1GiB"}, async (req, res) => {
+// 20 minutes timeout (!!)
+exports.genImageUrl = onRequest({secrets: [apiKey], timeoutSeconds: 1200, memory: "1GiB"}, async (req, res) => {
   const parsha:Parsha|null = await getTodaysParsha();
   if (!parsha) {
     console.error("Failed to get valid parsha data");
@@ -89,11 +90,35 @@ exports.genImageUrl = onRequest({secrets: [apiKey], timeoutSeconds: 500, memory:
 
   const results:string[] = [];
 
+  // generate an image for summary
+  const prompt = `Generate an image that illustrates the story of ${parsha.name}`;
+  const imageResult:ImageGenerationResult = await generateImageUrlAndSaveToFirestore(prompt, "summary", "image");
+  results.push(imageResult.imageUrl ?? "fail");
+  await setTimeout(65000); // sleep for 65 seconds to avoid quota of 1 query per minute
+
+  // generate an image for each theme
   for (const theme of parsha.themes) {
     const prompt = `Generate an image that illustrates the theme of ${theme} in ${parsha.name}`;
     const imageResult:ImageGenerationResult = await generateImageUrlAndSaveToFirestore(prompt, "themes", theme);
     results.push(imageResult.imageUrl ?? "fail");
     await setTimeout(65000); // sleep for 65 seconds to avoid quota of 1 query per minute
   }
+
+  // generate an image for each theme
+  for (const lesson of parsha.lessons) {
+    const prompt = `Generate an image that illustrates the lesson of ${lesson} as described in ${parsha.name}`;
+    const imageResult:ImageGenerationResult = await generateImageUrlAndSaveToFirestore(prompt, "lessons", lesson);
+    results.push(imageResult.imageUrl ?? "fail");
+    await setTimeout(65000); // sleep for 65 seconds to avoid quota of 1 query per minute
+  }
+
+  // generate an image for each theme
+  for (const point of parsha.keyPoints) {
+    const prompt = `Generate an image that illustrates ${point} as described in ${parsha.name}`;
+    const imageResult:ImageGenerationResult = await generateImageUrlAndSaveToFirestore(prompt, "keyPoints", point);
+    results.push(imageResult.imageUrl ?? "fail");
+    await setTimeout(65000); // sleep for 65 seconds to avoid quota of 1 query per minute
+  }
+
   res.json({result: `Images added with results: ${results} `});
 });
